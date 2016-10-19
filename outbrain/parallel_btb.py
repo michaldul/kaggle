91c9ae4aa672374dd82867dd581a790d0904cfcb
michaldul <michaldul@outlook.com>
2016-10-19 11:36:18 +0200

    pandas and dask performance measures

--- outbrain/parallel_btb.py
+++ outbrain/parallel_btb.py
@@ -1,9 +1,10 @@
-import dask.dataframe as dd
 
-reg = 10  # trying anokas idea of regularization
-
-
+# 2min 45s/3min 34s (with compute)
 def generate_unrolled_output():
+    import dask.dataframe as dd
+
+    reg = 10  # trying anokas idea of regularization
+
     train = dd.read_csv("./data/clicks_train.csv")
     cnt = train[train.clicked == 1].ad_id.value_counts()
     cntall = train.ad_id.value_counts()
@@ -12,7 +13,29 @@ def generate_unrolled_output():
     test = dd.read_csv("./data/clicks_test.csv")
     test = test.assign(prop=test['ad_id'].map(prop))
 
-    test.to_csv("./unrolled.csv", index=False)
+    test.compute().to_csv("./unrolled.csv", index=False)
+
+
+def ads_ordered(df):
+    return ' '.join(df.sort_values(by='prop', ascending=False)['ad_id'].astype(str))
+
+
+# 52min 33s
+def group_results():
+    import dask.dataframe as dd
+
+    dd.read_csv("./unrolled.csv").groupby('display_id')\
+        .apply(ads_ordered, meta=('ad_id', 'str'))\
+        .to_csv('./results.csv')
+
+# 49min 44s
+def group_results_pandas():
+    import pandas as pd
+
+    pd.read_csv("./unrolled.csv").groupby('display_id')\
+        .apply(ads_ordered)\
+        .to_csv('./results.csv')
+
 
 if __name__ == '__main__':
-    generate_unrolled_output()
+    group_results_pandas()
