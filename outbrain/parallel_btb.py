d34066ea5a53e74de75bcde2c48e3e84281268b7
michaldul <michaldul@outlook.com>
2016-10-20 23:42:17 +0200

    spark generating results in 3.5min

--- outbrain/parallel_btb.py
+++ outbrain/parallel_btb.py
@@ -37,5 +37,19 @@ def group_results_pandas():
         .to_csv('./results.csv')
 
 
+# PySpark approach - 3min 32s
+def sorted_by_prob(l):
+    l.sort(key=lambda pair: pair[1], reverse=True)
+    return ','.join([str(pair[0]) for pair in l])
+
+sc.textFile('c:\\dev\\kaggle\\outbrain\\unrolled_no_header.csv') \
+    .map(lambda row: row.split(',')) \
+    .map(lambda row: (int(row[0]), (int(row[1]), float(row[2] or 0)))) \
+    .aggregateByKey([], lambda t, r: t + [r], lambda a, b: a + b) \
+    .map(lambda row: "{0};{1}".format(row[0], sorted_by_prob(row[1]))) \
+    .saveAsTextFile('output_spark.csv')
+
+
+
 if __name__ == '__main__':
     group_results_pandas()
