5b8deb9ffe15af9ad0f1ecf4610980b576c964af
michaldul <michaldul@outlook.com>
2016-10-21 19:35:18 +0200

    fixes to pyspark btb

--- outbrain/pyspark_btb.py
+++ outbrain/pyspark_btb.py
@@ -1,5 +1,8 @@
 
+from pyspark import SparkContext
 from operator import add
+
+sc = SparkContext("local", "Outbrain BTB")
 
 TEST_CSV = 'c:\\dev\\kaggle\\outbrain\\data\\clicks_test.csv'
 TRAIN_CSV = 'c:\\dev\\kaggle\\outbrain\\data\\clicks_train.csv'
@@ -13,18 +16,18 @@ clicks_train = sc.textFile(TRAIN_CSV) \
 ad_clicks = clicks_train.reduceByKey(add)
 ad_shows = clicks_train.map(lambda ad: (ad[0], 1)).reduceByKey(add)
 ratios = ad_clicks.leftOuterJoin(ad_shows) \
-    .map(lambda row: (row[0], row[1][0]/(row[1][1] or 1)))
+    .map(lambda row: (row[0], row[1][0]/((row[1][1] or 1)+10)))
 
 def sorted_by_prob(l):
-    l.sort(key=lambda pair: pair[1], reverse=True)
-    return ','.join([str(pair[0]) for pair in l])
+    l.sort(key=lambda pair: pair[1] or 0, reverse=True)
+    return ' '.join([str(pair[0]) for pair in l])
 
 clicks_test = sc.textFile(TEST_CSV) \
     .filter(lambda line: line[0] != 'd') \
     .map(lambda line: line.split(',')) \
     .map(lambda row: (int(row[1]), int(row[0]))) \
-    .join(ratios) \
+    .leftOuterJoin(ratios) \
     .map(lambda row: (int(row[1][0]), (int(row[0]), row[1][1]))) \
     .aggregateByKey([], lambda t, r: t + [r], lambda a, b: a + b) \
-    .map(lambda row: "{0};{1}".format(row[0], sorted_by_prob(row[1]))) \
+    .map(lambda row: "{0},{1}".format(row[0], sorted_by_prob(row[1]))) \
     .saveAsTextFile(OUTPUT_CSV)
